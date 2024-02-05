4fce704d708dd7159bb38a29099c7b3f0495d995
Theo Stavrides <theostavrides@Theos-MacBook-Air.local>
2024-02-05 00:46:03 -0800

    works

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ async function fetchHTML(url){
 }
 
 // Converts HTML tabular data  JSON format
-async function parseHTML(html){
+function parseHTML(html){
     const $ = cheerio.load(html);
     const $tables = $('table')
-
+    const response = []
     
     for (let $table of $tables) {
         const output = []
@@ -25,10 +25,6 @@ async function parseHTML(html){
         for (let header of $headers) {
             const headerText = $(header).text().trim()
             headers.push(headerText)
-        }
-
-        if (headers.includes('Catalog')) {
-            continue
         }
 
         const $bodyRows = $('tbody tr', $table)
@@ -45,29 +41,90 @@ async function parseHTML(html){
                 output.push(rowOutput)
             }
         }
+
+        response.push({
+            headers,
+            data: output
+        })
+    }
+
+    return response
+}
+
+async function start() {
+    const pages = [
+        {
+            composer: 'Ludwig van Beethoven',
+            url: 'https://imslp.org/wiki/List_of_works_by_Ludwig_van_Beethoven',
+        },
+        {
+            composer: 'Frédéric Chopin',
+            url: 'https://imslp.org/wiki/List_of_works_by_Frédéric_Chopin'
+        },
+        {
+            composer: 'Wolfgang Amadeus Mozart',
+            url: 'https://imslp.org/wiki/List_of_works_by_Wolfgang_Amadeus_Mozart'
+        },
+        {
+            composer: 'Johann Sebastian Bach',
+            url: 'https://imslp.org/wiki/List_of_works_by_Johann_Sebastian_Bach'
+        },
+        {
+            composer: 'Pyotr Tchaikovsky',
+            url: 'https://imslp.org/wiki/List_of_works_by_Pyotr_Tchaikovsky'
+        },
+        {
+            composer: 'Johannes Brahms',
+            url: 'https://imslp.org/wiki/List_of_works_by_Johannes_Brahms',
+        },
+        {
+            composer: 'Antonio Vivaldi',
+            url: 'https://imslp.org/wiki/List_of_works_by_Antonio_Vivaldi',
+        },
+        {
+            composer: 'Joseph Haydn',
+            url: 'https://imslp.org/wiki/List_of_works_by_Joseph_Haydn',
+        },
+        {
+            composer: 'Gustav Mahler',
+            url: 'https://imslp.org/wiki/List_of_works_by_Gustav_Mahler',
+        },
+        {
+            composer: 'Franz Schubert',
+            url: 'https://imslp.org/wiki/List_of_works_by_Franz_Schubert',
+        },
+        {
+            composer: 'Felix Mendelssohn',
+            url: 'https://imslp.org/wiki/List_of_works_by_Felix_Mendelssohn',
+        },
+        {
+            composer: 'Antonín Dvořák',
+            url: 'https://imslp.org/wiki/List_of_works_by_Antonín_Dvořák',
+        },
+        {
+            composer: 'Sergei Rachmaninoff',
+            url: 'https://imslp.org/wiki/List_of_works_by_Sergei_Rachmaninoff',
+        },
+    ]
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
+    for (const page of pages) {
+        const { composer, url } = page
+        const html = await fetchHTML(url)
+        const parsed = parseHTML(html)
+        console.log('------------------------------------')
+        console.log('~')
+        console.log(composer)
+        console.log('~\n\n')
+        console.log(parsed)
+        await sleep(1000)
     }
 }
 
-// takes a JSON array of compositions and saves it to PSQL
-async function save({
-    compositions_json
-}){
+start()
 
-}
-
-// scrapes an IMSPL list of compositions by a composer and saves it do the db
-async function process({
-    composer,
-    url,
-}){
-    const html = await fetchHTML(url)
-    const json = parseHTML(html)
-}
-
-process({
-    composer: 'Ludwig van Beethoven',
-    url: 'https://imslp.org/wiki/List_of_works_by_Ludwig_van_Beethoven',
-    // url: 'https://imslp.org/wiki/List_of_works_by_Frédéric_Chopin',
-})
 
 
